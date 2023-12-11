d556bff1900589f6a6c967d0155be9bd5396286b
maulik8200 <mmonpara368@gmail.com>
2023-12-11 13:43:18 +0530

    Done

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+x = int(input("enter your No:"))
+
+if x % 2 == 0:
+    print(f"Your number {x} is even")
+else :
+    print(f"Your number {x} is odd")
---
+++ positive_nagative_zero.py
@@ -0,0 +1,8 @@
+x = int(input("enter your No:"))
+
+if x==0:
+    print(f"Your number {x} is Zero")
+elif x>0:
+    print(f"Your number {x} is Positive")
+elif x<0:
+    print(f"Your number {x} is Nagative")
